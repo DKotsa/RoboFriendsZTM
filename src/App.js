63d188cf91c15cbfd3fbcfcca17c3f3d575309fe
DKotsa <dimitris.kotsanitis@gmail.com>
2022-03-27 17:33:45 +0300

    A commit from Ubuntu

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import  { robots } from './robots';
+// import  { robots } from './robots';
 import './App.css';
 
 class App extends Component {
@@ -10,13 +10,18 @@ class App extends Component {
     constructor() {
         super();
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
     }
 
+    componentDidMount () {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => {this.setState({ robots : users })})
+    }
+
     onSearchChange = (event) => {
-        // console.log(event.target.value);
         this.setState({ searchfield : event.target.value }); 
     }
 
